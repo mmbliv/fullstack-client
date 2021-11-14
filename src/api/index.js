770a02dd3fa4c68edb1fd48b64eb511b9f5aa02c
Min <mmbliv@gmail.com>
2021-11-13 16:53:51 -0800

    deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:5000/posts";
+const url = "https://fullstack-post.herokuapp.com/posts";
 export const fetchPosts = () => axios.get(url);
 // export const fetchPost = (id) => axios.get(`${url}/${id}`);
 export const createPost = (newPost) => axios.post(url, newPost);
